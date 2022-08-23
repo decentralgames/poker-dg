6b6b7d82dcf44755576c9c971706d7fac54d122a
Kyle Gusdorf <kgusdorf@gmail.com>
2022-08-22 18:32:55 -0700

    run build

--- dist/lib/dealer.js
+++ dist/lib/dealer.js
@@ -222,7 +222,8 @@ var Dealer = /** @class */ (function () {
         if (((_b = (_a = this._bettingRound) === null || _a === void 0 ? void 0 : _a.numActivePlayers()) !== null && _b !== void 0 ? _b : 0) <= 1) {
             this._roundOfBetting = community_cards_1.RoundOfBetting.RIVER;
             // If there is only one pot, and there is only one player in it...
-            if (this._potManager.pots().length === 1 &&
+            if ((this._potManager.pots().length <= 1 ||
+                this._potManager.pots()[1].size() === 0) &&
                 this._potManager.pots()[0].eligiblePlayers().length === 1) {
                 // ...there is no need to deal the undealt community cards.
             }
@@ -262,7 +263,8 @@ var Dealer = /** @class */ (function () {
         assert_1.default(!this.bettingRoundInProgress(), 'Betting round must not be in progress');
         assert_1.default(this.bettingRoundsCompleted(), 'Betting rounds must be completed');
         this._handInProgress = false;
-        if (this._potManager.pots().length === 1 &&
+        if ((this._potManager.pots().length <= 1 ||
+            this._potManager.pots()[1].size() === 0) &&
             this._potManager.pots()[0].eligiblePlayers().length === 1) {
             // No need to evaluate the hand. There is only one player.
             var index = this._potManager.pots()[0].eligiblePlayers()[0];
